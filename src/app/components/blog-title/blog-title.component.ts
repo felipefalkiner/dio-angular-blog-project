b5fe93c298748ec8f7ced6cfb61a605a90a169bf
felipefalkiner <felipe.magosso@gmail.com>
2023-09-24 17:01:55 -0300

    created title Input on blog-title component

--- src/app/components/blog-title/blog-title.component.ts
+++ src/app/components/blog-title/blog-title.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
 @Component({
   selector: 'app-blog-title',
@@ -6,6 +6,7 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./blog-title.component.css']
 })
 export class BlogTitleComponent implements OnInit {
+  @Input() title: string = '';
 
   constructor() { }
 
