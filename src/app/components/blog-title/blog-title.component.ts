93f9fb124fe7eb66dcb7554630ac478b2e65b558
felipefalkiner <felipe.magosso@gmail.com>
2023-09-24 19:58:50 -0300

    example of adding multiple csses to a component

--- src/app/components/blog-title/blog-title.component.ts
+++ src/app/components/blog-title/blog-title.component.ts
@@ -3,7 +3,7 @@ import { Component, OnInit, Input } from '@angular/core';
 @Component({
   selector: 'app-blog-title',
   templateUrl: './blog-title.component.html',
-  styleUrls: ['./blog-title.component.css']
+  styleUrls: ['./blog-title.component.css','./blog-title.responsive.component.css']
 })
 export class BlogTitleComponent implements OnInit {
   @Input() title: string = '';
