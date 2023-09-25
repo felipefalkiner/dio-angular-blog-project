3452ece61b0c420bc71e68d7c211e0c92c35f186
felipefalkiner <felipe.magosso@gmail.com>
2023-09-25 13:14:58 -0300

    updates on pages\content component to receive Input data

--- src/app/pages/content/content.component.ts
+++ src/app/pages/content/content.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-content',
@@ -6,6 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./content.component.css']
 })
 export class ContentComponent implements OnInit {
+
+  @Input() photoCover:string = 'https://picsum.photos/850/350';
+  @Input() cardTitle:string = 'Temporary Title';
+  @Input() cardText:string = 'Lorem ipsum dolor sit, amet consectetur adipisicing elit. Animi ipsa nostrum esse assumenda quidem maxime, sed ut unde adipisci veniam labore modi ullam sunt tempore nemo voluptates accusantium? Vitae, hic!';
 
   constructor() { }
 
